3c7106ce65f4109cbdf05fec9a32d3f89a2229be
NihatBayramli <secondforever7@gmail.com>
2025-03-20 00:02:23 +0400

    Add files via upload

---
+++ age_group.py
@@ -0,0 +1,9 @@
+x=input("pleas write number: ")
+if 0<=int(x)<=12:
+    print("child")
+elif 13<=int(x)<=19:
+    print("teenager")
+elif 20<=int(x)<=64:
+    print("Adult")
+elif 65<=int(x):
+    print("Senior")
